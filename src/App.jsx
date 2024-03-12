628273da37951bb47f36d63b481f3141773480a9
smsaifurrahman <smsaifur525@gmail.com>
2024-03-12 12:19:58 +0100

    Created Blogs, Blog, Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
      
-      <h1 className='text-4xl bg-red-50'>Vite + React</h1>
+     <Header></Header>
+     <div className='md:flex container mx-auto'>
+          <Blogs></Blogs>
+          <Bookmarks></Bookmarks>
+     </div>
+     
      
     </>
   )
